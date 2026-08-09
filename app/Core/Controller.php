6541e3af27df895bafafaa9c8c4932e7aec900df
Fabian Cuellar <pcuellar366@gmail.com>
2026-08-09 02:20:50 -0400

    Fase 1 terminada creacion del archivo app/Core/Controller.php con la clase padre

---
+++ app/Core/Controller.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace App\Core;
+
+class Controller{
+    protected function json(array $data, int $status = 200): void
+    {
+        http_response_code($status);
+        header('content-type: application/json; charset=utf-8');
+        echo json_encode($data);
+        exit;
+    }
+
+    protected function getBody(): ?array
+    {
+        $input = file_get_contents('php://input');
+
+        return json_decode($input, true);
+    }
+
+    protected function handleError(string $message, int $status = 400): void
+    {
+        $this->json([
+            'success' => false, 
+            'error' => $message
+        ], $status);
+
+    }
+}

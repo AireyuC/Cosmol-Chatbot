f3a1733efff4c80dbedd03cbdcdb4a87c892639d
AireyuC <aireyucd@gmail.com>
2026-09-10 19:44:00 -0400

    arreglo de zona horaria

--- app/Presentacion/PlantillasWhatsApp/PlantillaFactura.php
+++ app/Presentacion/PlantillasWhatsApp/PlantillaFactura.php
@@ -32,8 +32,10 @@ class PlantillaFactura
             $contador = 1;
             foreach ($facturas as $f) {
                 $montoF = number_format($f['monto'], 2, ',', '.');
-                $timestamp = strtotime($f['fecha']);
-                $fechaF = $timestamp !== false ? date('d/m/Y', $timestamp) : $f['fecha'];
+                $fechaF = PlantillaSocio::formatearFechaLocal((string)($f['fecha'] ?? ''), 'd/m/Y');
+                if (empty($fechaF)) {
+                    $fechaF = (string)($f['fecha'] ?? '');
+                }
                 $mensajeTexto .= "$contador. Periodo: {$f['periodo']} - Monto: $montoF Bs. (Pagado el $fechaF)\n";
                 $contador++;
             }
